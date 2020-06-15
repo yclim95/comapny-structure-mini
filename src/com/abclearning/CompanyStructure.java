91916a2508f59b5559e9abad6c856dd1b0fe1225
yclim95 <lyc950421@gmail.com>
2020-06-15 12:53:21 +0800

    Update .java

--- src/com/abclearning/CompanyStructure.java
+++ src/com/abclearning/CompanyStructure.java
@@ -3,24 +3,25 @@ package com.abclearning;
 public class CompanyStructure {
 
     public static void main(String[] args) {
-        // Object Initialization
-        TechnicalLead CTO = new TechnicalLead("Satya Nadella");
-        SoftwareEngineer seA = new SoftwareEngineer("Kasey");
-        SoftwareEngineer seB = new SoftwareEngineer("Breana");
-        SoftwareEngineer seC = new SoftwareEngineer("Eric");
+        // Object Initialization (Tech Lead)
+        TechnicalLead CTO = new TechnicalLead("Gishella");
+        SoftwareEngineer seA = new SoftwareEngineer("Anastasya");
+        SoftwareEngineer seB = new SoftwareEngineer("Gempi");
+        SoftwareEngineer seC = new SoftwareEngineer("Marteen");
 
-        // Add ReportLine
+        // Add ReportLine (Tech)
         CTO.addReport(seA);
         CTO.addReport(seB);
         CTO.addReport(seC);
         System.out.println(CTO.getTeamStatus());
+        
 
         // Object Creation
-        TechnicalLead VPofENG = new TechnicalLead("Bill Gates");
-        SoftwareEngineer seD = new SoftwareEngineer("Winter");
-        SoftwareEngineer seE = new SoftwareEngineer("Libby");
-        SoftwareEngineer seF = new SoftwareEngineer("Gizan");
-        SoftwareEngineer seG = new SoftwareEngineer("Zaynah");
+        TechnicalLead VPofENG = new TechnicalLead("Sabyan");
+        SoftwareEngineer seD = new SoftwareEngineer("Berwyn");
+        SoftwareEngineer seE = new SoftwareEngineer("Poppy");
+        SoftwareEngineer seF = new SoftwareEngineer("I Wayan");
+        SoftwareEngineer seG = new SoftwareEngineer("Nadine");
 
         //Add Report Line
         VPofENG.addReport(seD);
@@ -30,9 +31,9 @@ public class CompanyStructure {
         System.out.println(VPofENG.getTeamStatus());
 
         //Object Creation
-        BusinessLead CFO = new BusinessLead("Amy Hood");
-        Accountant actA = new Accountant("Niky");
-        Accountant actB = new Accountant("Andrew");
+        BusinessLead CFO = new BusinessLead("Christiant");
+        Accountant actA = new Accountant("Laurensky");
+        Accountant actB = new Accountant("Jessica");
 
         //Add Report Line
         CFO.addReport(actA, CTO);
